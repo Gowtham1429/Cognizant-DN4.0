c44141469339c63f36f6269bd4219b01de4a3cf9
Gowtham1429 <sanipinasaigowtham@gmail.com>
2025-06-16 22:12:00 +0530

    Update Gowtham_SQL_HandsOn.sql

--- Gowtham_SQL_HandsOn.sql
+++ Gowtham_SQL_HandsOn.sql
@@ -1,12 +1,9 @@
--- Create your test database
 CREATE DATABASE GowthamTestDB;
 GO
 
--- Use that database
 USE GowthamTestDB;
 GO
 
--- Create a Students table
 CREATE TABLE Students (
     StudentID INT PRIMARY KEY,
     Name VARCHAR(50),
@@ -14,12 +11,10 @@ CREATE TABLE Students (
 );
 GO
 
--- Insert data into Students table
 INSERT INTO Students (StudentID, Name, Age)
 VALUES 
 (1, 'Gowtham', 21),
 (2, 'Sai', 22);
 GO
 
--- Retrieve all records from the table
 SELECT * FROM Students;
