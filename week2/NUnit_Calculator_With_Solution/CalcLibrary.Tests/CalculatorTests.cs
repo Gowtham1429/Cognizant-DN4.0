342c5a6637503a54e8f3c02e83ff52df75848fcf
Gowtham1429 <sanipinasaigowtham@gmail.com>
2025-06-29 14:48:09 +0530

    Add Week 2 SQL assignment files

---
+++ week2/NUnit_Calculator_With_Solution/CalcLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
+        [Test]
+        [TestCase(2, 3, 5)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-5, 5, 0)]
+        public void Add_ValidInputs_ReturnsExpected(int a, int b, int expected)
+        {
+            Assert.That(calc.Add(a, b), Is.EqualTo(expected));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calc = null!;
+        }
+    }
+}
