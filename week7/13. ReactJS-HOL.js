e35ff5adc4fc2b12369cd1bec3cffe2a57390d05
Gowtham1429 <sanipinasaigowtham@gmail.com>
2025-08-10 17:43:00 +0530

    week7

---
+++ week7/13. ReactJS-HOL.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>applications running...</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ week7/16. ReactJS-HOL.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>App running...</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
